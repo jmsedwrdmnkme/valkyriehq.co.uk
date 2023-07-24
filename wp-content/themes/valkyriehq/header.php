cd9f0715d4b4ce7e3d403ad4146dba21b8fcd128
James Monk <james@jamesmonk.me>
2023-07-24 13:16:28 +0200

    Nav html

--- wp-content/themes/valkyriehq/header.php
+++ wp-content/themes/valkyriehq/header.php
@@ -10,6 +10,22 @@
 <a class="visually-hidden-focusable" href="#main">Skip to main content</a>
 
 <header>
-  <nav>
+  <nav class="navbar navbar-expand-lg">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main-menu" aria-controls="main-menu" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="main-menu">
+      <?php
+        wp_nav_menu(array(
+          'theme_location' => 'main-menu',
+          'container' => false,
+          'menu_class' => '',
+          'fallback_cb' => '__return_false',
+          'items_wrap' => '<ul id="%1$s" class="navbar-nav %2$s">%3$s</ul>',
+          'depth' => 2,
+          'walker' => new bootstrap_5_wp_nav_menu_walker()
+        ));
+      ?>
+    </div>
   </nav>
 </header>
