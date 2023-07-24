9f1dae68b4321e94a1facfdc7cce4a38f6363e64
James Monk <james@jamesmonk.me>
2023-07-24 13:38:24 +0200

    Footer menu update

--- wp-content/themes/valkyriehq/footer.php
+++ wp-content/themes/valkyriehq/footer.php
@@ -1,9 +1,16 @@
 <footer>
   <nav>
-    <?php wp_nav_menu( array( 
-      'theme_location' => 'footer-menu',
-      'depth' => 1,
-    ) ); ?>
+    <?php
+      wp_nav_menu(array(
+        'theme_location' => 'footer-menu',
+        'container' => false,
+        'menu_class' => '',
+        'fallback_cb' => '__return_false',
+        'items_wrap' => '<ul id="%1$s" class="navbar-nav %2$s">%3$s</ul>',
+        'depth' => 1,
+        'walker' => new bootstrap_5_wp_nav_menu_walker()
+      ));
+    ?>
   </nav>
 </footer>
 
