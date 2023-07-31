6bfdb86a058c2780cf2721f1fbac091db2493249
James Monk <james@jamesmonk.me>
2023-07-31 21:44:55 +0200

    Glide JS update for sliders

--- src/js/script.js
+++ src/js/script.js
@@ -4,22 +4,26 @@ import { collapse } from 'bootstrap'
 // Glide carousel
 import Glide, { Controls, Breakpoints } from '@glidejs/glide/dist/glide.modular.esm.js'
 
-new Glide('.glide', {
-  type: 'carousel',
-  focus: 'center',
-  gap: 192,
-  breakpoints: {
-    1399: {
-      gap: 176
-    },
-    1199: {
-      gap: 144
-    },
-    991: {
-      gap: 144
-    },
-    767: {
-      gap: 112
+const sliders = document.querySelectorAll('.glide');
+
+sliders.forEach((slider) => {
+  new Glide(slider, {
+    type: 'carousel',
+    focus: 'center',
+    gap: 192,
+    breakpoints: {
+      1399: {
+        gap: 176
+      },
+      1199: {
+        gap: 144
+      },
+      991: {
+        gap: 144
+      },
+      767: {
+        gap: 112
+      }
     }
-  }
-}).mount({ Controls, Breakpoints })
+  }).mount({ Controls, Breakpoints })
+});
