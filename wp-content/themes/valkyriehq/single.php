f05de10cdb982b393b51a6d4aee1fbc665b30c9d
James Monk <james@jamesmonk.me>
2023-08-01 09:47:05 +0200

    Quick inner blog template update

--- wp-content/themes/valkyriehq/single.php
+++ wp-content/themes/valkyriehq/single.php
@@ -1,8 +1,23 @@
 <?php get_header(); ?>
 
 <main id="main">
-  <h1>Single</h1>
-  <?php the_content(); ?>
+  <div class="bg-secondary">
+    <div class="container">
+      <div class="h1"><?php echo esc_html(get_the_title()); ?></div>
+    </div>
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md my-5">
+        <?php the_content(); ?>
+      </div>
+      <aside class="col-md-3 col-lg-2 my-5">
+        <div class="bg-secondary p-5">
+          ASIDE
+        </div>
+      </aside>
+    </div>
+  </div>
 </main>
 
 <?php get_footer(); ?>
