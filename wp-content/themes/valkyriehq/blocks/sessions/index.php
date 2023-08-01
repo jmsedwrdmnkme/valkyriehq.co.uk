d9bb679db21d53d19423c3b87516b826bcde5da2
James Monk <james@jamesmonk.me>
2023-08-01 12:25:06 +0200

    Tagline update to sessions block

--- wp-content/themes/valkyriehq/blocks/sessions/index.php
+++ wp-content/themes/valkyriehq/blocks/sessions/index.php
@@ -20,7 +20,7 @@
     <div class="row justify-content-center">
       <?php
         $args = array(
-          'post_type' => 'sessions',
+          'post_type' => 'session',
           'posts_per_page' => 3
         );
         $the_query = new WP_Query($args);
@@ -29,12 +29,14 @@
           $image_id = get_the_ID();
           $image_alt = get_post_meta(get_post_thumbnail_id(), '_wp_attachment_image_alt', TRUE);
           $image_src = get_the_post_thumbnail_url($image_id, 'column-16x9');
+
+          $tagline = get_the_field('tagline');
       ?>
         <div class="col-md-6 col-lg-4 mt-4">
           <a href="<?php the_permalink(); ?>" class="d-block position-relative text-white">
             <div class="bottom-0 left-0 m-3 position-absolute z-1 pe-none">
               <h3 class="bg-black d-inline-block mb-0 px-1"><?php the_title(); ?></h3>
-              <small class="bg-black col-lg-10 d-inline-block px-1">Lorem ipsum sit dolor amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</small>
+              <small class="bg-black col-lg-10 d-inline-block px-1"><?php echo tagline; ?></small>
             </div>
             <img loading="lazy" src="<?php echo $image_src; ?>" alt="<?php echo $image_alt; ?>" class="img-fluid w-100" width="735" height="415">
           </a>
@@ -49,7 +51,7 @@
     <div class="mt-4 pt-3 text-center pb-3">
       <a href="<?php echo get_site_url(); ?>/sessions/" class="btn btn-outline-primary">Find out more</a>
     </div>
-  <?php else; ?>
+  <?php else : ?>
     <div class="pb-3"></div>
   <?php endif; ?>
 </div>
