f52fe56443af11c43b81d95bf0eccd4c17e34d61
James Monk <james@jamesmonk.me>
2023-08-01 11:15:46 +0200

    Get started component

--- wp-content/themes/valkyriehq/blocks/get-started/index.php
+++ wp-content/themes/valkyriehq/blocks/get-started/index.php
@@ -2,18 +2,20 @@
   /**
    * Get Started Block Template.
    */
+
+  $title = get_field('title');
+  $text = get_field('text');
 ?>
 <div class="component__book py-4 bg-primary">
   <div class="container">
     <div class="row align-items-center justify-content-between">
       <div class="col-lg-5 col-xl-4 text-center text-lg-start pb-1">
-        <h2 class="display-5">Get started training with us</h2>
-        <a href="#" class="btn btn-outline-light mt-3">Any questions?</a>
-        <a href="#" class="btn btn-dark mt-3 ms-3">Book a session</a>
+        <h2 class="display-5"><?php echo $title; ?></h2>
+        <a href="<?php echo get_site_url(); ?>/contact-us/" class="btn btn-outline-light mt-3">Any questions?</a>
+        <a href="<?php echo get_site_url(); ?>/book-a-session/" class="btn btn-dark mt-3 ms-3">Book a session</a>
       </div>
       <div class="col-lg-6 col-xl-7 mt-4 lead mt-lg-3">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <?php echo $text; ?>
       </div>
     </div>
   </div>
