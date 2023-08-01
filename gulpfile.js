8d53bb9a9eaf797e0251c5df1d43cb1a4e397840
James Monk <james@jamesmonk.me>
2023-08-01 10:18:44 +0200

    Static/Browsersync removed for local dev now theme is underway. CSS for blocks updated, excess CSS removed

--- gulpfile.js
+++ gulpfile.js
@@ -17,58 +17,31 @@ import hb from 'gulp-hb';
 import ext from 'gulp-ext-replace'
 import fs from 'fs';
 import path from 'path';
-import browsersync from 'browser-sync';
 
 export const clean = () => del([ 'wp-content/themes/valkyriehq/' ]);
 export const cleanComponents = () => del([ 'wp-content/themes/valkyriehq/template-parts/' ]);
 
 export function fonts() {
-  gulp.src('src/fonts/*')
-    .pipe(gulp.dest('wp-content/themes/valkyriehq/static/fonts/'))
-    .pipe(browsersync.stream());
   return gulp.src('src/fonts/*')
-    .pipe(gulp.dest('wp-content/themes/valkyriehq/fonts/'))
-    .pipe(browsersync.stream());
+    .pipe(gulp.dest('wp-content/themes/valkyriehq/fonts/'));
 }
 
 export function scripts() {
-  gulp.src('src/js/script.js')
-    .pipe(webpack({}, compiler, function(err, stats) {}))
-    .pipe(uglify())
-    .pipe(concat('script.js'))
-    .pipe(gulp.dest('wp-content/themes/valkyriehq/static/js'))
-    .pipe(browsersync.stream());
   return gulp.src('src/js/script.js')
     .pipe(webpack({}, compiler, function(err, stats) {}))
     .pipe(uglify())
     .pipe(concat('script.js'))
-    .pipe(gulp.dest('wp-content/themes/valkyriehq/'))
-    .pipe(browsersync.stream());
+    .pipe(gulp.dest('wp-content/themes/valkyriehq/'));
 }
 
 export function styles() {
-  gulp.src('src/scss/style.scss')
-    .pipe(sass({outputStyle: 'compressed'}))
-    .pipe(cleanCSS())
-    .pipe(gulp.dest('wp-content/themes/valkyriehq/static/'))
-    .pipe(browsersync.stream());
   return gulp.src('src/scss/style.scss')
     .pipe(sass({outputStyle: 'compressed'}))
     .pipe(cleanCSS())
-    .pipe(gulp.dest('wp-content/themes/valkyriehq/'))
-    .pipe(browsersync.stream());
+    .pipe(gulp.dest('wp-content/themes/valkyriehq/'));
 }
 
 export function sprite() {
-  gulp.src('src/sprite/**/**/*.svg')
-    .pipe(svgsprite({
-      shape: { spacing: { padding: 5 } },
-      mode: { symbol: true },
-      svg: { xmlDeclaration: false, doctypeDeclaration: false, namespaceIDs: false, namespaceClassnames: false }
-    }))
-    .pipe(concat('sprite.hbs'))
-    .pipe(gulp.dest('src/html/static/partials/global/'))
-    .pipe(browsersync.stream());
   return gulp.src('src/sprite/**/**/*.svg')
     .pipe(svgsprite({
       shape: { spacing: { padding: 5 } },
@@ -76,35 +49,10 @@ export function sprite() {
       svg: { xmlDeclaration: false, doctypeDeclaration: false, namespaceIDs: false, namespaceClassnames: false }
     }))
     .pipe(concat('sprite.hbs'))
-    .pipe(gulp.dest('src/html/partials/global/'))
-    .pipe(browsersync.stream());
+    .pipe(gulp.dest('src/html/partials/global/'));
 }
 
 export function images() {
-  gulp.src('src/img/**/**/*')
-    .pipe(imagemin([
-      gifsicle({interlaced: true}),
-      mozjpeg({quality: 75, progressive: true}),
-      optipng({optimizationLevel: 5}),
-      svgo({
-        plugins: [
-          {
-            name: 'removeViewBox',
-            active: true
-          },
-          {
-            name: 'cleanupIDs',
-            active: true
-          },
-          {
-            name: 'collapseGroups',
-            active: true
-          }
-        ]
-      })
-    ]))
-    .pipe(gulp.dest('wp-content/themes/valkyriehq/static/img/'))
-    .pipe(browsersync.stream());
   return gulp.src('src/img/**/**/*')
     .pipe(imagemin([
       gifsicle({interlaced: true}),
@@ -127,21 +75,14 @@ export function images() {
         ]
       })
     ]))
-    .pipe(gulp.dest('wp-content/themes/valkyriehq/img/'))
-    .pipe(browsersync.stream());
+    .pipe(gulp.dest('wp-content/themes/valkyriehq/img/'));
 }
 
 export function html() {
-  gulp.src('src/html/static/*.hbs')
-    .pipe(hb().partials('src/html/static/partials/**/*.hbs'))
-    .pipe(ext('.html'))
-    .pipe(gulp.dest('wp-content/themes/valkyriehq/static/'))
-    .pipe(browsersync.stream());
   return gulp.src('src/html/*.hbs')
     .pipe(hb().partials('src/html/partials/**/*.hbs'))
     .pipe(ext('.php'))
-    .pipe(gulp.dest('wp-content/themes/valkyriehq/'))
-    .pipe(browsersync.stream());
+    .pipe(gulp.dest('wp-content/themes/valkyriehq/'));
 }
 
 export function getFolders(dir) {
@@ -164,22 +105,19 @@ export function components() {
         return gulp.src(componentsSrc + '/style.scss', { allowEmpty: true })
           .pipe(sass({outputStyle: 'compressed'}))
           .pipe(cleanCSS())
-          .pipe(gulp.dest(componentsDest))
-          .pipe(browsersync.stream());
+          .pipe(gulp.dest(componentsDest));
       },
 
       function componentsHtml () {
         return gulp.src(componentsSrc + '/index.hbs', { allowEmpty: true })
           .pipe(ext('.php'))
-          .pipe(gulp.dest(componentsDest))
-          .pipe(browsersync.stream());
+          .pipe(gulp.dest(componentsDest));
       },
 
       function componentsCopy (done) {
         return gulp
           .src([componentsSrc + '/*', componentsSrcNot + '/*.hbs', componentsSrcNot + '/*.scss'])
-          .pipe(gulp.dest(componentsDest))
-          .pipe(browsersync.stream());
+          .pipe(gulp.dest(componentsDest));
       },
 
       function componentsDone (done) {
@@ -187,16 +125,6 @@ export function components() {
       }
     );
   });
-}
-
-export function browserSync(done) {
-  browsersync.init({server: {baseDir: "wp-content/themes/valkyriehq/static/"}, port: 3000});
-  done();
-}
-
-export function browserSyncReload(done) {
-  browsersync.reload();
-  done();
 }
 
 function watchFiles() {
@@ -210,6 +138,6 @@ function watchFiles() {
 
 const componentsWatch = gulp.series(cleanComponents, gulp.parallel.apply(gulp.parallel, components()));
 export const build = gulp.series(clean, gulp.parallel(fonts, sprite, images, scripts, styles), html, componentsWatch);
-const watch = gulp.series(build, browserSync, watchFiles);
+const watch = gulp.series(build, watchFiles);
 
 export default watch;
