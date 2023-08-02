e2d0bcea1b8b00a96d0c14e1de12e932a87dcfc3
James Monk <james@jamesmonk.me>
2023-08-02 09:59:56 +0200

    FAQs block

--- wp-content/themes/valkyriehq/blocks/faqs/index.php
+++ wp-content/themes/valkyriehq/blocks/faqs/index.php
@@ -2,79 +2,30 @@
   /**
    * FAQs Block Template.
    */
-
-  global $post;
-
-  $title = get_field('title');
-  $text = get_field('text');
-  $pages = get_field('pages_to_show');
-
 ?>
 <div class="component__faqs py-5">
   <div class="container text-center">
-    <div class="h2 display-5"><?php echo $title; ?></div>
-    <?php if ($text) : ?>
-      <div class="border-2 border-primary border-top lead mt-4 pt-3">
-        <?php echo $text; ?>
+    <h2 class="display-5">FAQs</h2>
+  </div>
+  <?php if(have_rows('faqs')) : ?>
+    <div class="container-fluid mt-1 mb-3">
+      <div class="accordion accordion-flush" id="accordionFaqs">
+        <?php while(have_rows('faqs')) : the_row();
+          $question = get_sub_field('question');
+          $answer = get_sub_field('answer');
+        ?>
+          <div class="accordion-item">
+            <h2 class="accordion-header">
+              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                Accordion Item #1
+              </button>
+            </h2>
+            <div id="flush-collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionFaqs">
+              <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the first item's accordion body.</div>
+            </div>
+          </div>
+        <?php endwhile; ?>
       </div>
     <?php endif; ?>
-  </div>
-  <div class="container-fluid mt-1">
-    <div class="row justify-content-center">
-      <?php if($pages) :
-        foreach($pages as $page) :
-          $page_link = get_permalink($page->ID);
-          $page_title = get_the_title($page->ID);
-          $page_image_alt = get_post_meta($page->ID, '_wp_attachment_image_alt', TRUE);
-          $page_image_src = get_the_post_thumbnail_url($page->ID, 'column-16x9');
-      ?>
-          <div class="col-md-6 col-lg-4 mt-4">
-            <a href="<?php echo esc_url($page_link); ?>" class="d-block position-relative text-white">
-              <div class="top-50 start-50 position-absolute translate-middle z-1 pe-none">
-                <h2 class="h3 bg-black d-inline-block px-1"><?php echo $page_title; ?></h2>
-              </div>
-              <img loading="lazy" src="<?php echo $page_image_src; ?>" alt="<?php echo $page_image_alt; ?>" class="img-fluid w-100" width="735" height="415">
-            </a>
-          </div>
-        <?php endforeach; ?>
-      <?php endif; ?>
-    </div>
-  </div>
-  <div class="container">
-    <div class="border-2 border-primary border-top mt-5 pt-3">
-    </div>
-  </div>
-</div>
-
-<div class="accordion accordion-flush" id="accordionFlushExample">
-  <div class="accordion-item">
-    <h2 class="accordion-header">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-        Accordion Item #1
-      </button>
-    </h2>
-    <div id="flush-collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-      <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the first item's accordion body.</div>
-    </div>
-  </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-        Accordion Item #2
-      </button>
-    </h2>
-    <div id="flush-collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-      <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the second item's accordion body. Let's imagine this being filled with some actual content.</div>
-    </div>
-  </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-        Accordion Item #3
-      </button>
-    </h2>
-    <div id="flush-collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-      <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
-    </div>
   </div>
 </div>
