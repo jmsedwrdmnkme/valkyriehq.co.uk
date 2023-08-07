ea3b36fce624a8ce05e2855afe0a762a515aaffb
James Monk <james@jamesmonk.me>
2023-08-07 20:02:32 +0200

    Contact page

--- wp-content/themes/valkyriehq/blocks/page-contact/index.php
+++ wp-content/themes/valkyriehq/blocks/page-contact/index.php
@@ -18,7 +18,7 @@
     </div>
     <div class="map col-lg mt-4">
       <div class="ratio ratio-16x9">
-        <iframe loading="lazy" src="https://maps.google.com/maps?q=<?php echo map_address; ?>&amp;t=&amp;z=10&amp;ie=UTF8&amp;iwloc=&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+        <iframe loading="lazy" src="https://maps.google.com/maps?q=<?php echo $map_address; ?>&amp;t=&amp;z=10&amp;ie=UTF8&amp;iwloc=&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
       </div>
     </div>
   </div>
