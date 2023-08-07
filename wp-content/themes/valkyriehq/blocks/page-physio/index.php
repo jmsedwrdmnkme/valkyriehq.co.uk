04269d6dd8ca69c775721fb78ac94235de9effdf
James Monk <james@jamesmonk.me>
2023-08-07 13:00:03 +0200

    Physio page

--- wp-content/themes/valkyriehq/blocks/page-physio/index.php
+++ wp-content/themes/valkyriehq/blocks/page-physio/index.php
@@ -8,39 +8,34 @@
   $show_button = get_field('show_button_to_sessions_page');
 ?>
 <div class="component__page--physio">
-  <?php
-    $args = array(
-      'post_type' => 'session'
-    );
-    $the_query = new WP_Query($args);
-    while ($the_query -> have_posts()) : 
-      $the_query -> the_post();	  
-      $image_id = get_the_ID();
-      $image_alt = get_post_meta(get_post_thumbnail_id(), '_wp_attachment_image_alt', TRUE);
-      $image_src = get_the_post_thumbnail_url($image_id, 'column-16x9');
-  ?>
-  <div class="component__page--sessions-row pt-4 pb-5">
-    <div class="container-fluid">
-      <div class="row align-items-center justify-content-center">
-        <div class="image col-lg mt-4">
-          <img loading="lazy" src="<?php echo $image_src; ?>" alt="<?php echo $image_alt; ?>" class="img-fluid w-100" width="735" height="415">
-        </div>
-        <div class="content col-lg mt-4">
-          <div class="content--container">
-            <a href="<?php the_permalink(); ?>">
-              <h2 class="display-5"><?php the_title(); ?></h2>
-            </a>
-            <p class="lead"><?php the_field('tagline', get_the_ID()) ?></p>
-            <a href="<?php the_permalink(); ?>" class="btn btn-outline-primary mt-2">Find out more</a>
-            <a href="<?php echo get_site_url(); ?>/book-a-session/" class="btn btn-primary mt-2 ms-2">Book a session</a>
+  <?php if(have_rows('content_block')) : ?>
+    <?php while(have_rows('content_block')) : the_row();
+      $image_id = get_sub_field('image');
+      $image_alt = get_post_meta($image_id, '_wp_attachment_image_alt', TRUE);
+      $image_src = wp_get_attachment_image_src($image_id, 'column-16x9')[0];
+
+      $title = get_sub_field('title');
+      $content = get_sub_field('content');
+    ?>
+      <div class="component__page--sessions-row pt-4 pb-5">
+        <div class="container-fluid">
+          <div class="row align-items-center justify-content-center">
+            <div class="image col-lg mt-4">
+              <img loading="lazy" src="<?php echo $image_src; ?>" alt="<?php echo $image_alt; ?>" class="img-fluid w-100" width="735" height="415">
+            </div>
+            <div class="content col-lg mt-4">
+              <div class="content--container">
+                <a href="<?php the_permalink(); ?>">
+                  <h2 class="display-5"><?php echo $title; ?></h2>
+                </a>
+                <div>
+                  <?php echo $content; ?>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </div>
-  <?php
-    endwhile;
-    wp_reset_postdata();
-  ?>
+    <?php endwhile; ?>
+  <?php endif; ?>
 </div>
-
