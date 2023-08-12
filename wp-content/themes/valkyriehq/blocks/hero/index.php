a551bd63751e70da14b6132987fb7f787ad010f3
James Monk <james@jamesmonk.me>
2023-08-12 22:46:15 +0200

    COaches post type template

--- wp-content/themes/valkyriehq/blocks/hero/index.php
+++ wp-content/themes/valkyriehq/blocks/hero/index.php
@@ -14,10 +14,10 @@
     $link_target = $button_link['target'] ? $button_link['target'] : '_self';
   }
 
-  $image_id = $post->ID;
+  $post_id = $post->ID;
   $image_alt = get_post_meta(get_post_thumbnail_id(), '_wp_attachment_image_alt', TRUE);
-  $image_desktop = get_the_post_thumbnail_url($image_id, 'hero-home-desktop');
-  $image_mobile = get_the_post_thumbnail_url($image_id, 'hero-home-mobile');
+  $image_desktop = get_the_post_thumbnail_url($post_id, 'hero-home-desktop');
+  $image_mobile = get_the_post_thumbnail_url($post_id, 'hero-home-mobile');
 ?>
 <div class="component__hero align-items-center d-flex justify-content-center position-relative overflow-hidden py-5">
   <div class="container my-5 py-5 position-relative z-1 text-center">
@@ -30,10 +30,10 @@
       </div>
       <?php elseif (is_singular('coach')) : ?>
       <?php
-        $role = get_field('role');
-        $links = get_field('links');
+        $role = get_field($post_id, 'role');
+        $links = get_field($post_id, 'links');
       ?>
-      <div class="text-lg-start col-lg-6 ms-lg-auto">
+      <div class="text-lg-start col-lg-6 ms-lg-auto d-lg-flex justify-content-end">
         <div class="col-lg-auto d-inline-block">
           <div class="h1 display-3 mt-5 border-primary border-bottom border-2 pb-3"><?php the_title(); ?></div>
           <div class="mt-3 h5 text-uppercase"><?php echo $role; ?></div>
