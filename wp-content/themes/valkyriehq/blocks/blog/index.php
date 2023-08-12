c5f57a28eb6aca9526518f234c736189fa03b235
James Monk <james@jamesmonk.me>
2023-08-12 20:12:35 +0200

    Offset blog block posts displayed

--- wp-content/themes/valkyriehq/blocks/blog/index.php
+++ wp-content/themes/valkyriehq/blocks/blog/index.php
@@ -41,7 +41,12 @@
       <div class="col-lg-6 d-none d-md-block">
         <div class="row">
           <?php
-            $the_query = new WP_Query( 'posts_per_page=4' );
+             $args = array(
+               'post_type'     => 'post',
+               'showposts'     => 4,
+               'offset'        => 1,
+             );
+            $the_query = new WP_Query($args);
             while ($the_query -> have_posts()) : 
               $the_query -> the_post();
               $image_id = get_the_ID();
