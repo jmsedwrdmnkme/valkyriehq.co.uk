a66daf5515d8b588cb4ae0e59dfed79bf3c66e45
James Monk <james@jamesmonk.me>
2023-08-12 20:35:57 +0200

    COaches post type template

--- wp-content/themes/valkyriehq/single-coach.php
+++ wp-content/themes/valkyriehq/single-coach.php
@@ -1,7 +1,10 @@
 <?php get_header(); ?>
 
 <main id="main">
+  <?php echo do_blocks('<!-- wp:acf/hero {"name":"acf/hero","data":{"field_64c92d131c364":"","field_64c92d131c3a7":{"title":"","url":"","target":""}},"mode":"preview"} /-->'); ?>
   <?php the_content(); ?>
+  <?php echo do_blocks('<!-- wp:block {"ref":209} /-->'); ?>
+  <?php echo do_blocks('<!-- wp:block {"ref":212} /-->'); ?>
 </main>
 
 <?php get_footer(); ?>
