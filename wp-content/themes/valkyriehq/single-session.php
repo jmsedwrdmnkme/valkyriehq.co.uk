a1eeeecc87a480af5725d0dafb21b61e81585f13
James Monk <james@jamesmonk.me>
2023-08-13 12:06:14 +0200

    Session inner

--- wp-content/themes/valkyriehq/single-session.php
+++ wp-content/themes/valkyriehq/single-session.php
@@ -10,9 +10,9 @@
   <?php echo do_blocks('<!-- wp:acf/hero {"name":"acf/hero","data":{"field_64c92d131c364":"","field_64c92d131c3a7":{"title":"","url":"","target":""}},"mode":"preview"} /-->'); ?>
   <?php the_content(); ?>
 
-  <div class="container">
-    <div class="row align-items-center">
-      <div class="col-md-10 col-lg-8 col-xl-6">
+  <div class="container mt-n5">
+    <div class="row justify-content-center">
+      <div class="col-11 col-lg-8">
         <button type="button" class="ratio ratio-16x9 border-0 btn-video" data-bs-toggle="modal" data-bs-target="#videoModal">
           <img class="opacity-75" src="<?php echo $youtube_thumbnail; ?>" alt="Session intro video thumbnail" width="1280" height="720">
           <span class="d-none">Play session intro video</span>
